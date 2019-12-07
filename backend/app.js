e3e5db48467ee41ffe60c2a194a5b41abad3a08f
bastronaut <btsijtsma@gmail.com>
2019-12-07 13:26:04 +0100

    Adds item rendering capabilities, play buttons

--- backend/app.js
+++ backend/app.js
@@ -6,5 +6,6 @@ const port = 3000;
 app.use(cors());
 app.use(require('./controllers'));
 
+app.use(express.static('../frontend'));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
--- frontend/js/bundle.js
+++ frontend/js/bundle.js
@@ -21,17 +21,29 @@ const getPodcasts = () => {
 const renderPodcasts = () => {
     getPodcasts().then(podcasts => {
         if (podcasts.item) {
-            renderPodcastItem(podcasts.item[0]);
+            for (var i = 0; i < 50; i++) {
+                renderPodcastItem(podcasts.item[i], i);
+            }
         }
+
+        setupOnclicks();
 
     }).catch(err => console.log(err));
 }
 
-renderPodcastItem = (item) => {
+renderPodcastItem = (item, i) => {
     console.log(item);
+    const printNumber = (i + 1) + ".";
     const date = new Date(item.pubDate[0]);
     const printDate = formatDate(date);
-    console.log(printDate);
+    let clone = $("#js-podcast-item-template").clone();
+    clone.find("#js-number-indicator").text(printNumber);
+    clone.find("#js-title").text(printDate);
+    clone.attr("id", "");
+    clone.attr("audio-src", item.link);
+    clone.attr("audio-description", item.description);
+    $("#js-podcast-items").append(clone);
+    clone.show();
 }
 
 
@@ -51,6 +63,73 @@ const formatDate = (date) => {
 }
 
 renderPodcasts();
+
+
+const hidePodcastItems = () => {
+    $("#js-podcast-items").hide();
+}
+const showPodcastItems = () => {
+    $("#js-podcast-items").show();
+}
+
+const hidePodcastView = () => {
+    $("#js-podcast-view").hide();
+}
+const showPodcastView = () => {
+    $("#js-podcast-view").show();
+}
+
+const clickPlayButton = () => {
+    $("#js-play-button").hide();
+    $("#js-pause-button").show();
+    document.getElementById("js-audio-player").play();
+}
+
+const clickStopButton = () => {
+    $("#js-play-button").show();
+    $("#js-pause-button").hide();
+    document.getElementById("js-audio-player").pause();
+}
+
+const clickReturnToOverview = () => {
+    hidePodcastView();
+    showPodcastItems();
+
+}
+
+hidePodcastItems();
+showPodcastView();
+
+
+
+
+const setupOnclicks = () => {
+
+    $(".podcast-item-container").on("click", (e) => {
+        console.log(5);
+        console.log(this);
+        console.log(e);
+    })
+
+
+    $("#js-play-button").on("click", () => {
+        clickPlayButton();
+    })
+
+    $("#js-pause-button").on("click", () => {
+        clickStopButton();
+    })
+
+    $("#js-return-to-overview").on("click", () => {
+        clickReturnToOverview();
+    })
+}
+
+
+
+$(window).click(function (e) {
+    console.log(e.target); // then e.srcElement.className has the class
+});
 },{"./constants":1,"browser-request":3}],3:[function(require,module,exports){
 // Browser Request
 //
