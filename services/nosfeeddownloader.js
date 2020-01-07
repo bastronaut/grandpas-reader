b20e5a3c7ed687f699f217fc37c3baf0e16352e0
bastronaut <btsijtsma@gmail.com>
2020-01-07 21:25:45 +0100

    Start work on caching feeds

--- services/nosfeeddownloader.js
+++ services/nosfeeddownloader.js
@@ -1,6 +1,15 @@
 const axios = require('axios');
 const constants = require('../utils/constants')
 
+// cache
+const feeds = {};
+
+const a = {
+    "news": {
+        "timestamp": "asd"
+    }
+};
+
 const downloadFeed = (feedUrl) => {
     return axios.get(feedUrl);
 }
@@ -12,18 +21,45 @@ const downloadMetOogOpMorgen = (feedUrl) => {
 
 // NOS has multiple feeds... get 5 feeds and group them?
 const downloadNOSNews = () => {
-    const algemeen = downloadFeed("http://feeds.nos.nl/nosnieuwsalgemeen");
-    const politiek = downloadFeed("http://feeds.nos.nl/nosnieuwspolitiek");
 
-    return Promise.all([algemeen, politiek]).then(function (values) {
-        console.log(values.length);
-        return values;
-        // return new Promise((resolve, reject) => {
-        // resolve(values);
-        // });
-    })
+    if (shouldRefreshFeed(constants.NOS_NEWS_CACHE)) {
+
+        console.log("Should refresh the feed");
+        const algemeen = downloadFeed(constants.NOS_ALGEMEEN);
+        const politiek = downloadFeed(constants.NOS_POLITIEK);
+
+        return Promise.all([algemeen, politiek]).then(function (values) {
+            setFeedCacheForFeed(constants.NOS_NEWS_CACHE, values);
+            return values;
+        })
+    } else {
+        // Refresh not required, return from cache
+        console.log("No need to refresh feed");
+        return new Promise((resolve, reject) => {
+            resolve(feeds[constants.NOS_NEWS_CACHE].feedData);
+        });
+    }
 }
 
+const shouldRefreshFeed = (feed) => {
+    // Feed not yet in cache, go fetch
+    if (!feeds[feed]) {
+        console.log("feeds not yet in cache, fetching: " + feed);
+        return true;
+    }
+
+    const lastRefreshed = feeds[feed].timestamp;
+    const now = new Date().getTime();
+    return (now > lastRefreshed + constants.CACHE_REFRESH_DELTA);
+}
+
+const setFeedCacheForFeed = (feed, feedData) => {
+    console.log("setting feed for");
+    console.log(feed);
+    feeds[feed] = {};
+    feeds[feed].timestamp = new Date().getTime();
+    feeds[feed].data = feedData;
+}
 
 exports.downloadFeed = downloadFeed;
 exports.downloadMetOogOpMorgen = downloadMetOogOpMorgen;
--- utils/constants.js
+++ utils/constants.js
@@ -1,1 +1,6 @@
 exports.NOS_OOG_OP_MORGEN_PODCAST = "https://podcast.npo.nl/feed/met-het-oog-op-morgen.xml";
+exports.NOS_ALGEMEEN = "http://feeds.nos.nl/nosnieuwsalgemeen";
+exports.NOS_POLITIEK = "http://feeds.nos.nl/nosnieuwspolitiek";
+// exports.CACHE_REFRESH_DELTA = 1 * 60 * 60 * 1000; // 1 hour
+exports.CACHE_REFRESH_DELTA = 10 * 1000; // 
+exports.NOS_NEWS_CACHE = "NOSNews";
